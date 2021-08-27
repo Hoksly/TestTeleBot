05705c6d50142be1a29dbe4a5647c2974438a5ab
hoksly <realhoksly@gmail.com>
2021-08-27 12:31:56 +0300

    V.1.0.3 release

---
+++ set_up_bot.py
@@ -0,0 +1,19 @@
+import os
+import sqlite3
+from data.config import WEATHER_FORECAST_FOLDER, WEATHER_DB_ADDRES
+
+
+def create_dir(folder_path):
+    try:
+        os.mkdir(folder_path)
+    except:
+        pass
+
+
+def create_db(db_path):
+    sqlite3.connect(db_path)
+
+
+create_dir('data/databse/weather_databse/db')
+create_dir(WEATHER_FORECAST_FOLDER)
+create_db(WEATHER_DB_ADDRES)
