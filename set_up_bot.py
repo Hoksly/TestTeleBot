ed519f6548bb9be041b7309e09776eb5b89bfbe2
hoksly <realhoksly@gmail.com>
2021-08-27 12:34:07 +0300

    V.1.0.3 release

--- set_up_bot.py
+++ set_up_bot.py
@@ -14,6 +14,6 @@ def create_db(db_path):
     sqlite3.connect(db_path)
 
 
-create_dir('data/databse/weather_databse/db')
+create_dir('data/database/weather_databse/db')
 create_dir(WEATHER_FORECAST_FOLDER)
 create_db(WEATHER_DB_ADDRES)
